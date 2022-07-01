174cca938b6b3d839aa4fde58a1496049a5a3c13
devcesarindra <caugustoa@minsait.com>
2022-06-30 23:38:33 -0300

    special movie promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -36,6 +36,11 @@ public class Program {
                 if(capturedPiece != null) {
                     captured.add(capturedPiece);
                 }
+                if(chessMatch.getPromoted() != null) {
+                    System.out.println("Enter piece for promotion (B/T/C/R)");
+                    String type = sc.nextLine();
+                    chessMatch.replacePromotedPiece(type);
+                }
             } catch (ChessException e) {
                 System.out.println(e.getMessage());
                 sc.nextLine();
